5c4d686c868b941dbd8b1d1b63d840f468ae6b75
Fran Casas <nflamel@gmail.com>
2025-02-23 22:09:53 +0100

    docs: remember to remove unneeded stuff!

--- lib/settings/model.ts
+++ lib/settings/model.ts
@@ -1,5 +1,9 @@
 import type { Rule } from "lib/rules";
 
+// NOTE(franciscoj): [On 23/02/2025] this looks pretty overengineered like
+// this. I kept it from a previous version where I had different types of rules
+// and my next step is to implement more, so I'm keeping it, but if after a
+// couple of weeks it is still here and not used, remember to remove it!!
 interface Settings {
 	rules: Rule[];
 }
